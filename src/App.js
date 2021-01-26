c57839a4ea39d7ad566ebb025414914e394a0cd4
jequezinho <jroverby92@gmail.com>
2021-01-26 16:04:28 -0600

    Made drawTiles() function to identify which tiles player played, then draws new tiles to replenish rack

--- src/App.js
+++ src/App.js
@@ -8,14 +8,32 @@ import Player from './components/Player';
 function App() {
   const bag = createBag();
   const board = createBoard();
-  let playerUp = 0;
+  const playerInfo = {
+    playerUp: 0,
+    0: {
+      name: 'Jack Overby',
+      rack: bag.splice(0,7),
+      score: 0
+    },
+    1: {
+      name: 'Jason Vaysberg',
+      rack: bag.splice(0, 7),
+      score: 0
+    }
+  }
 
-  const [rack1, rack2] = [bag.splice(0,7), bag.splice(0, 7)];
+  function drawTiles(playerNo, tilesPlayed) {
+    let playerRack=playerInfo[playerNo]["rack"];
+    const playedIndices=tilesPlayed.map(playedTile=>playerRack.find((ix, tile)=>tile.letter===playedTile.letter));
+    playedIndices.forEach(playedIx=>playerRack.splice(playedIx, 1));
+    const newTilesDrawn=bag.splice(0, Math.min(7-tilesPlayed.length, bag.length));
+    newTilesDrawn.forEach(tile=>playerRack.push(tile));
+  }
 
   return (
     <div className="App">
-      <Player name={'Jack Overby'} rack={rack1}></Player>
-      <Player name={'Jason Vaysberg'} rack={rack2}></Player>
+      <Player player={playerInfo[0]}></Player>
+      <Player player={playerInfo[1]}></Player>
       <Board board={board} />
       <Bag bag={bag} />
     </div>
